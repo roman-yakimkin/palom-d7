1b6b33ab314a9ad97d81cd72ba5ba8c98ea67d52
Roman Yakimkin <r.yakimkin@yandex.ru>
2019-03-15 15:37:19 +0300

    Small modifications added

--- sites/all/themes/palom/template.php
+++ sites/all/themes/palom/template.php
@@ -4,3 +4,9 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function palom_system_info_alter(&$info, $file, $type){
+  if ($type == 'theme' && module_exists('tfd7') && ($info['name'] === 'bootstrap' || $info['base theme'] === 'bootstrap')) {
+    $info['engine'] = 'twig';
+  }
+}
