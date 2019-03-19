f127b2e5b2ab8d01334bef58310086b6d25b1e0a
Roman Yakimkin <r.yakimkin@yandex.ru>
2019-03-19 21:56:11 +0300

    The field for saving country region and city was added

---
+++ sites/all/modules/custom/palom_field_cr_city/field-geo-cr-city-default-formatter.tpl.php
@@ -0,0 +1,10 @@
+<p>
+    @if (!empty($element['city_url']))
+        {!! l($element['city_name'], $element['city_url']) !!}
+        @else
+        {!! $element['city_name'] !!}
+    @endif
+
+    ({{ $element['country_name'] }}@if (!empty($element['region_name'])), {{ $element['region_name'] }}@endif)
+
+</p>
