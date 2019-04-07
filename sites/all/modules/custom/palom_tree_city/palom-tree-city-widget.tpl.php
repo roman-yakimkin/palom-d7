24bcd8ee585e5bf05c62ad584cb76a1fa7c41cb8
Roman Yakimkin <r.yakimkin@yandex.ru>
2019-04-07 20:17:31 +0300

    Some css and views were improved

--- sites/all/modules/custom/palom_tree_city/palom-tree-city-widget.tpl.php
+++ sites/all/modules/custom/palom_tree_city/palom-tree-city-widget.tpl.php
@@ -10,11 +10,13 @@
             <div class="palom-tree-city-tree" id="palom-tree-city-{{ $field_name }}">
             </div>
         </div>
-        <div class="palom-tree-city-buttons col-xs-3">
-            <button class="add-city btn btn-default form-control">{{ t('Add a city') }}</button>
-            <button class="remove-city btn btn-default form-control">{{ t('Remove a city') }}</button>
+        <div class="palom-tree-city-buttons col-xs-2">
+            <div>
+                <button class="add-city btn btn-default form-control">{{ t('Add') }}</button>
+                <button class="remove-city btn btn-default form-control">{{ t('Remove') }}</button>
+            </div>
         </div>
-        <div class="palom-tree-city-listbox col-xs-4">
+        <div class="palom-tree-city-listbox col-xs-5">
             <select class="form-control form-select" name="sel_cities" size="10">
             </select>
         </div>
